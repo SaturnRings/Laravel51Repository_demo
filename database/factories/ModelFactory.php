683378aa313a0b43b337294a109ac6a39e34bb4d
oomusou <oomusou@gmail.com>
2015-12-08 15:45:45 +0800

    1. route新增/users 2. ModelFactory新增age欄位 3. migration新增age欄位 4. blade新增age欄位

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,10 @@
 
 $factory->define(MyBlog\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
+        'name'           => $faker->name,
+        'email'          => $faker->email,
+        'age'            => $faker->randomNumber(2),
+        'password'       => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
     ];
 });
